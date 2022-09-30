3eccc07faae8b3c7111ac4f7ceadaca624af4bbb
unknown <sampatikori.xworkz@gmail.com>
2022-09-30 22:43:49 +0530

    First push

---
+++ LogicalAndoperator.java
@@ -0,0 +1,10 @@
+class LogicalAndoperator{
+ 
+public static void main(String [] args){
+int a = 50;
+int b = 60;
+boolean result = (a<b) && (a<(b++));
+System.out.println(result);
+System.out.println("b : "+b);
+}
+}

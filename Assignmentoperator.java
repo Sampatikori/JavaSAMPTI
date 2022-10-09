dcec80045effee7f706d087acfb9f7ffe7600b05
unknown <sampatikori.xworkz@gmail.com>
2022-10-09 21:05:19 +0530

    sam

---
+++ Assignmentoperator.java
@@ -0,0 +1,11 @@
+class Assignmentoperator{
+ 
+public static void main(String [] args){
+int a=40;
+int b=20;
+a+=10;//a=a+10(a=40+10)
+b+=20;//b=b+20(b=20+20)
+System.out.println(a);
+System.out.println(b);
+}
+}

609e6e55a03c2aaadf6f42ff7a9b06cc415952b8
unknown <sampatikori.xworkz@gmail.com>
2022-10-11 22:08:38 +0530

    sam

---
+++ DoWhileLoopdemo.java
@@ -0,0 +1,10 @@
+class DoWhileLoopdemo{
+public static void main(String[] args){
+   int count=0;
+ do{
+  System.out.println("Hi Sam");
+  count++;
+}
+while(count<4);
+}
+}
---
+++ NestedForLoop4.java
@@ -0,0 +1,13 @@
+class NestedForLoop4{
+public static void main (String[] args){
+
+      for(int i=0; i<4; i++){
+      for(int j=0; j<4; j++){
+      for(int k=0; k<4; k++){
+   
+      System.out.println(k);
+}
+}
+}
+}
+}

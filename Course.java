0b8b659232ff00a0fdc6447ebd9ceb926f487b34
unknown <sampatikori.xworkz@gmail.com>
2022-10-12 22:42:01 +0530

    sam

---
+++ Course.java
@@ -0,0 +1,12 @@
+class Course{
+public static void main (String[] args){
+  learnFramework();
+}
+public static void learnFramework(){
+  learnJava();
+System.out.println("I learnt Framework");
+}
+public static void learnJava(){
+System.out.println("I learnt Java");
+}
+}
